9ce8c84c07985e71d264c4d882a13b361e01a0b5
CarloSchool <carlomiguelh@yahoo.com>
2012-12-12 13:27:19 -0600

    12/12/12

--- PrimeExperiment/src/Prime.java
+++ PrimeExperiment/src/Prime.java
@@ -1,22 +1,32 @@
+import java.util.Arrays;
+
 
 public class Prime {
 	private int limit;
 	private int list[];
+	
 	public Prime(int lim){
 		limit=lim;
-		list=new int[lim+1];
-		list[0]=1;
-		list[1]=2;
+		list=new int[lim];
+		list[0]=2;
 	}
-	private void calc(){
-		int count=0;
-		while(count<lim){
-			if(isPrime(Count))
+	public int[] calc(){
+		int count=1, testNum=3;
+		while(count<limit){
+			if(isPrime(testNum/*,count*/)){
+				list[count]=testNum;
+				//System.out.println(testNum);
+				count++;
+			}
+			testNum+=2;
 		}
+		return list;
 	}
 	private boolean isPrime(int x){
-		for(int x=0;x<list.length;x++)
-			if(x%list[x]==0)return false;
+		//for(int y=0;list[y+1]!=0;y++)
+		//	if(x%list[y]==0)return false;
+		for(int y=2;y<=Math.sqrt(x);y++)
+			if(x%y==0)return false;
 		return true;
 	}
 	
@@ -27,7 +37,15 @@ public class Prime {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = (int) System.currentTimeMillis();
+		Prime meh = new Prime(1000000);
+		meh.calc();
+		//System.out.println(Arrays.toString(meh.calc()));
+		
+		a = (int) System.currentTimeMillis()- a;
+		System.out.println(a);
+		
+		
 	}
 
 }
