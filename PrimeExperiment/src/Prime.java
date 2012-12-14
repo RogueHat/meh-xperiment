b17854608aeeb11e309ea5d212da5eaf7b490e09
CarloSchool <carlomiguelh@yahoo.com>
2012-12-13 23:08:03 -0600

    gottaGoFast

--- PrimeExperiment/src/Prime.java
+++ PrimeExperiment/src/Prime.java
@@ -1,30 +1,40 @@
 import java.util.Arrays;
 
 public class Prime {
-	private int limit;
+	private static int limit, sq, count, testNum;
 	private static int list[];
 
 	public Prime(int lim) {
+		reset(lim);
+	}
+	public void reset(int lim){
 		limit = lim;
 		list = new int[lim];
 		list[0] = 2;
+		list[1] = 3;
+		sq = (int) Math.sqrt(5);
+		count = 2;
+		testNum=5;
 	}
 
 	public void calc() {
-		int count = 1, testNum = 3;
+		// int count = 1, testNum = 3;
 		while (count < limit) {
-			if (isPrime(testNum)) {
+			if (isPrime()) {
 				list[count] = testNum;
 				count++;
 			}
 			testNum += 2;
+			sq = (int) Math.sqrt(testNum);
 		}
 	}
 
-	private boolean isPrime(int x) {
-		int a = (int) Math.sqrt(x);
-		for (int y = 3; y <= a; y += 2)
-			if (x % y == 0)
+	private boolean isPrime() {
+
+		// for (int y = 3; y <= sq; y += 2)
+		// if (x % y == 0) return false;
+		for (int y = 1; y < count && list[y] <= sq; y++)
+			if (testNum % list[y] == 0)
 				return false;
 		return true;
 	}
@@ -33,11 +43,16 @@ public class Prime {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int a = (int) System.currentTimeMillis();
-		Prime meh = new Prime((int) 1E6);
-		meh.calc();
-		a = (int) System.currentTimeMillis() - a;
-		System.out.print(a+"\t");
-		//System.out.println(Arrays.toString(list));
+		int ha = (int) 1E6;
+		Prime meh = new Prime(ha);
+		
+		//while(true){
+			long a =  System.currentTimeMillis();
+			meh.calc();
+			a =  System.currentTimeMillis() - a;
+			System.out.println(a + "\t");
+			// System.out.println(Arrays.toString(list));
+			meh.reset(ha);
+		//}
 	}
 }
