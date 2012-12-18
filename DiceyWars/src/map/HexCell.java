c940c4cf6ac681eeec6e731c413a6e407e2d1976
CarloSchool <carlomiguelh@yahoo.com>
2012-12-18 13:25:28 -0600

    not done

--- DiceyWars/src/map/HexCell.java
+++ DiceyWars/src/map/HexCell.java
@@ -4,12 +4,14 @@ import java.awt.Color;
 
 public class HexCell {
 	private int x,y;
+	private boolean isEvenRow;
 	private Color clr;
 	
 	public HexCell(int xp,int yp){
 		x=xp;
 		y=yp;
 		clr=Color.white;
+		if(int y);
 	}
 	public HexCell(int xp,int yp,Color color){
 		this(xp,yp);
@@ -20,5 +22,12 @@ public class HexCell {
 	}
 	public Color getClr(){
 		return clr;
+	}
+	
+	public HexCell left(HexCell[][] meh){
+		return meh[x-1][y];
+	}
+	public HexCell right(HexCell[][] meh){
+		return meh[x+1][y];
 	}
 }
